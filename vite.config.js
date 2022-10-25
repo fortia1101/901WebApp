d1e4348c4e4f519bb94975be19306cccf68e45b3
fortia1101 <masa00.vr3@gmail.com>
2022-10-25 15:18:13 +0900

    fix: vite.config.jsを修正してmanifest.jsonを再出力

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,19 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/welcome.css',
+                'resources/css/mypage.css',
+                'resources/css/archive-show.css',
+                'resources/css/delete-confirm.css',
+                'resources/css/flash_message.css',
+                'resources/css/header.css',
+                'resources/css/home.css',
+                'resources/css/admin/header.css',
+                'resources/css/admin/dashboard.css',
+                // 'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
